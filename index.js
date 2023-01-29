d10f8c9d0de669e1945a842529da5f31114f6dc7
aletson <me@ajl.io>
2023-01-29 14:45:44 -0500

    add EmbedBuilder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const {SlashCommandBuilder, GatewayIntentBits, Partials, PermissionsBitField, PermissionFlagsBits} = require('discord.js');
+const {EmbedBuilder, SlashCommandBuilder, GatewayIntentBits, Partials, PermissionsBitField, PermissionFlagsBits} = require('discord.js');
 const client = new Discord.Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages], partials: [Partials.Message, Partials.Channel, Partials.Reaction], });
 var mysql = require('mysql2');
 var fetch = require('node-fetch');
