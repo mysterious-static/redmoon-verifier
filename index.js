dc42b234150a485b84471174ee5075125c68582b
aletson <me@ajl.io>
2023-01-29 11:40:09 -0500

    change

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.on('ready', async () => {
         .setRequired(true))
       .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
       
-      console.log(command.data.toJSON());
+      console.log(command.data);
     //await client.application.commands.set([data]);
     //}
 });
