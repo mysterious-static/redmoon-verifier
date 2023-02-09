88a636c7624e273c28a1b74df3bcdc753d17d9d1
aletson <me@ajl.io>
2023-02-08 19:32:55 -0500

    fix for now

--- index.js
+++ index.js
@@ -252,9 +252,9 @@ client.on('messageCreate', async function (message) {
       var messageCount = await message.channel.messages.fetch({ after: isStickyChannel.last_message_id });
       console.log(messageCount.size);
       if (messageCount.size >= isStickyChannel.speed) {
-        await message.channel.messages.fetch(isStickyChannel.last_message_id).then(message => message.delete());
         var sentMessage = await message.channel.send({ content: isStickyChannel.message }); // Post sticky message
         await connection.promise().query('update stickymessages set last_message_id = ? where channel_id = ?', [sentMessage.id, isStickyChannel.channel_id]);
+        await message.channel.messages.fetch(isStickyChannel.last_message_id).then(message => message.delete()); // TODO check if message exists
         stickymessages = await connection.promise().query('select * from stickymessages'); // Refresh the live cache
       }
     }
