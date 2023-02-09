ff761049baf03fda178d10e558ef7d1ffec873d4
aletson <me@ajl.io>
2023-02-09 08:26:45 -0500

    change what happens async

--- index.js
+++ index.js
@@ -252,10 +252,9 @@ client.on('messageCreate', async function (message) {
         var messageCount = await message.channel.messages.fetch({ after: isStickyChannel.last_message_id });
         console.log(messageCount.size);
         if (messageCount.size >= isStickyChannel.speed) {
-          await message.channel.messages.fetch(isStickyChannel.last_message_id).then(message => message.delete()).catch((error) => { console.error(error) }); // TODO check if message exists
+          message.channel.messages.fetch(isStickyChannel.last_message_id).then(message => message.delete()).catch((error) => { console.error(error) }); // TODO check if message exists
           var sentMessage = await message.channel.send({ content: isStickyChannel.message }); // Post sticky message
-          await connection.promise().query('update stickymessages set last_message_id = ? where channel_id = ?', [sentMessage.id, isStickyChannel.channel_id]);
-          stickymessages = await connection.promise().query('select * from stickymessages'); // Refresh the live cache
+          var stickymessages = connection.promise().query('update stickymessages set last_message_id = ? where channel_id = ?; select * from stickymessages;', [sentMessage.id, isStickyChannel.channel_id])[1];
         }
       }
     } else {
