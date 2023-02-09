380fd29e2a9984925baa75d52841b9b80fc6a186
aletson <me@ajl.io>
2023-02-08 22:55:03 -0500

    asfd

--- index.js
+++ index.js
@@ -140,7 +140,9 @@ client.on('interactionCreate', async (interaction) => {
 });
 
 client.on('messageCreate', async function (message) {
-  await message.fetch();
+  if(message.partial) {
+    await message.fetch()
+  }
 
   if (message.content.startsWith('!rm ')) {
     if (message.guild.ownerId != message.author.id) {
