75c25c5057a4d37f0057916e57a3012fc9b73881
aletson <me@ajl.io>
2023-05-23 20:18:41 -0400

    asdf

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ client.on('interactionCreate', async (interaction) => {
               ]
             }
           },
-          CallerReference: new Date.valueOf()
+          CallerReference: new Date().valueOf()
         }
         var cf = new CloudFrontClient({ credentials: fromIni({ profile: "redmoon" }) });
         command = new CreateInvalidationCommand(params);
@@ -323,7 +323,7 @@ client.on('interactionCreate', async (interaction) => {
         var cf = new CloudFrontClient({ credentials: fromIni({ profile: "redmoon" }) });
         var params = {
           DistributionConfig: {
-            CallerReference: new Date.valueOf(),
+            CallerReference: new Date().valueOf(),
             Origins: {
               Items: [
                 {
@@ -433,7 +433,7 @@ client.on('interactionCreate', async (interaction) => {
               ContinuousDeploymentPolicyId: "",
               Staging: false,
               PriceClass: 'PriceClass_All',
-              CallerReference: new Date.valueOf(),
+              CallerReference: new Date().valueOf(),
               Origins: {
                 Items: [
                   {
