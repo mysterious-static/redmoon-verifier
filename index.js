23eace9c51d88645467fa27f77d1a08b76ac7152
aletson <me@ajl.io>
2023-07-24 10:59:56 -0400

    asdf

--- index.js
+++ index.js
@@ -1093,9 +1093,9 @@ client.on('interactionCreate', async (interaction) => {
 
         // THIS PART DIDNT WORK
         console.log(role[0][0].role_id);
-        var users = await interaction.guild.roles.get(role[0][0].role_id).members;
+        var users = await interaction.guild.roles.cache.get(role[0][0].role_id);
         console.log(users);
-        for (const user of users) {
+        for (const user of users.members) {
           await thread.members.add(user.id);
         }
 
